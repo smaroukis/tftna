bd7321744f6c513e2d742848112812fa77b41cad
smarouki <smarouki@umich.edu>
2019-03-08 17:07:18 -0800

    working auth flow

--- gspread_auth.py
+++ gspread_auth.py
@@ -1,10 +1,13 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json'), scope)
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('sa-client_secret.json', scope)
 
-sheet_name = '<Sheet_name>'
+client = gspread.authorize(creds)
+
+sheet_name = 'TFTNA Log (Responses)'
 sheet = client.open(sheet_name).sheet1
 
-
+all_records = sheet.get_all_records()
+print(all_records)
