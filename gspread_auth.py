dd23ab9f6bd1048c2cb8bf9aa37cb320bfdbe4d9
smarouki <smarouki@umich.edu>
2019-03-08 17:38:26 -0800

    gspread sandboxing

--- gspread_auth.py
+++ gspread_auth.py
@@ -1,13 +1,19 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('sa-client_secret.json', scope)
+def get_sheet():
+    ## Auth Flow
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('sa-client_secret.json', scope)
 
-client = gspread.authorize(creds)
+    client = gspread.authorize(creds)
 
-sheet_name = 'TFTNA Log (Responses)'
-sheet = client.open(sheet_name).sheet1
+    sheet_name = 'TFTNA Log (Responses)'
+    sheet = client.open(sheet_name).sheet1
 
-all_records = sheet.get_all_records()
-print(all_records)
+    return sheet
+    # all_records = sheet.get_all_records()
+    # returns list of dictionaries for each row with {col1: value, col2:val2, etc}
+    # sheet.findall("Strength")
+    # 
+    
